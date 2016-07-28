50c09f9f5542a53038fa8cd13c7d6c52a4ca1ba3
Jordan Campbell <jcampbell1441@gmail.com>
2016-07-28 12:32:45 -0500

    about to setup redirect to swipe page

--- server/index.js
+++ server/index.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var browserify = require('browserify-middleware');
-var Path = require('path');
+var path = require('path');
 var fetch = require('isomorphic-fetch');
 var bodyParser = require('body-parser');
 
-var Routes = express.Router();
 var app = express();
 
 var port = process.env.PORT || 4000;
@@ -29,31 +28,31 @@ app.get('/app-bundle.js',
 );
 
 //
-// Example endpoint (also tested in test/server/index_test.js)
-//
-routes.get('/api/tags-example', function(req, res) {
-  res.send(['node', 'express', 'browserify', 'mithril'])
-})
-
-routes.use( '/api', require('./apis/pets-api') )
-
-//
 // Github Authorization
 //
 
 app.get('/auth/login', (req, res) => {
-  console.log(req);
-  console.log(req.param('code'))
-  fetch('https://github.com/login/oauth/access_token?client_id=444a46dcbe1340ce4a49&client_secret=df1f3fc9a5da7f88c06a4432302c42d04ac8f151&code=' + req.param('code') + '&redirect_uri=http://localhost:4000/swipe', {
+  // console.log(req);
+  // console.log(req.param('code'))
+  console.log("Running");
+
+  fetch('https://github.com/login/oauth/access_token?client_id=444a46dcbe1340ce4a49&client_secret=df1f3fc9a5da7f88c06a4432302c42d04ac8f151&code=' + req.param('code'), {
     method: 'POST',
-    Accept: 'application/json'
+    headers: {
+      Accept: 'application/json'
+    }
   })
-  .then(result => {
-    console.log("Result from github auth:", result.body)
-    
+  .then(response => {
+    // console.log("response from github auth:", response)
+    return response.json()
     //fetch()
 
     //res.redirect('/swipe');
+  })
+  .then(result => {
+    console.log("result:", result);
+    res.cookie("AuthToken", result.access_token).redirect('/swipe');
+  })
 });
 
 // Wild card route for client side routing.
@@ -64,38 +63,9 @@ app.get('/*', function(req, res){
 //
 // Static assets (html, etc.)
 //
-var assetFolder = Path.resolve(__dirname, '../client/public')
-routes.use(express.static(assetFolder))
+var assetFolder = path.resolve(__dirname, '../client/public')
 
 
-if (process.env.NODE_ENV !== 'test') {
-  //
-  // The Catch-all Route
-  // This is for supporting browser history pushstate.
-  // NOTE: Make sure this route is always LAST.
-  //
-  routes.get('/*', function(req, res){
-    res.sendFile( assetFolder + '/index.html' )
-  })
-
-  //
-  // We're in development or production mode;
-  // create and run a real server.
-  //
-  var app = express()
-
-  // Parse incoming request bodies as JSON
-  app.use( require('body-parser').json() )
-
-  // Mount our main router
-  app.use('/', routes)
-
-  // Start the server!
-  var port = process.env.PORT || 4000
-  app.listen(port)
-  console.log("Listening on port", port)
-}
-else {
-  // We're in test mode; make this file importable instead.
-  module.exports = routes
-}
+var port = process.env.PORT || 4000
+app.listen(port)
+console.log("Listening on port", port)
