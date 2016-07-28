3d09831594e51428ed370fb825887f5bf1e4fbaa
Jordan Campbell <jcampbell1441@gmail.com>
2016-07-28 17:37:10 -0500

    Adding profile and working on skills

---
+++ client/components/Profile.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { browserHistory, Link } from 'react-router';
+import fetch from 'isomorphic-fetch';
+
+import * as model from '../models/profile';
+
+export default class Profile extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			userInfo: {
+				username: null,
+				location: null,
+				bio: null,
+				avatar: null,
+			},
+			userSkills: ["React", "Node", "Express"],
+		}
+	}
+
+	componentWillMount() {
+		model.getUserData(document.cookie.split('=')[1])
+		.then(res => {
+			this.setState({userInfo: res});
+		})
+	}
+
+  render() {
+	  return (
+	    <div className="profile">
+	     	<div>
+	     		<img src={this.state.userInfo.avatar_url} />
+	     		<h1>{this.state.userInfo.login}</h1>
+	     		<div>{this.state.userInfo.location}</div>
+	     		<div>Followers: {this.state.userInfo.followers}</div>
+	     		<p>{this.state.userInfo.bio}</p>
+	     	</div>
+	     	<div className="skills">
+	     		{this.state.userSkills.map(skill => {
+	     			<div className="skill">
+							{skill}
+						</div>
+	     		})}
+	     	</div>
+	    </div>
+	  )
+	}
+}
+
+// export default class Skill extends React.Component {
+// 	constructor(props){
+// 		super(props);
+// 	}
+
+// 	render() {
+// 		return (
+// 			<div className="skill">
+// 				{this.props.skillName}
+// 			</div>
+// 			)
+// 	}
+// }
--- client/components/Swipe.js
+++ client/components/Swipe.js
@@ -16,27 +16,12 @@ export default class Swipe extends React.Component {
 		}
 	}
 
-	componentWillMount() {
-		model.getUserData(document.cookie.split('=')[1])
-		.then(res => {
-			this.setState({userInfo: {
-				username: res.login,
-				location: res.location,
-				bio: res.bio,
-				avatar: res.avatar_url
-			}})
-		})
-	}
-
   render() {
 	  return (
 	    <div>
 	     	Welcome to GITLUV!
 	     	<div>
-	     		<img src={this.state.userInfo.avatar} />
-	     		<h1>{this.state.userInfo.username}</h1>
-	     		<div>{this.state.userInfo.location}</div>
-	     		<p>{this.state.userInfo.bio}</p>
+	     		<button type="button"><Link to={`profile`}>Profile</Link></button>
 	     	</div>
 	    </div>
 	  )
