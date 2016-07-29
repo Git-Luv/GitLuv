c156b83333f1fb9faf29583cae3ddeff2d2002dc
Kyhan Turner <kyhanpt@gmail.com>
2016-07-29 10:48:42 -0500

    render project info, add like/dislike buttons

--- client/components/Swipe.js
+++ client/components/Swipe.js
@@ -8,13 +8,21 @@ export default class Swipe extends React.Component {
 		super(props);
 		this.state = {
 			projects: [
-				{ title: 'atlantis',
+				{ id: 1,
+					title: 'atlantis',
 					description: 'building an underwater digital civilization',
 					lookingFor: 'programmer who can hold their breath very long',
 					skills: 'h20 tolerant'
+				},
+				{ id: 2,
+					title: 'rocketman',
+					description: 'creating a javascript spaceship to go populate mars',
+					lookingFor: 'rocketman with backend savvy to store our digital supplies',
+					skills: 'not prone to motion sickness'
 				}
 			]
 		}
+		this.handleDislike = this.handleDislike.bind(this);
 	}
  // componentWillMount() {
  // 	fetchProjects()
@@ -23,6 +31,16 @@ export default class Swipe extends React.Component {
  // 		});
  //  }
 
+ 	handleDislike(event) {
+ 		event.preventDefault();
+ 		console.log(event)
+ 		console.log('state', this.state.projects)
+ 		var updatedProjects = this.state.projects.slice().shift()
+		this.setState({ projects: updatedProjects })
+		console.log(this.state.projects)
+ 	}
+
+
   render() {
 	  return (
 	  	<div className='swipe'>
@@ -30,7 +48,7 @@ export default class Swipe extends React.Component {
 		     	{ this.state.projects
 		     		.map((project) => {
 		     		return (
-		     			<div className='currentProject'>
+		     			<div key='project.id' className='currentProject'>
 				     		<h1>{project.title}</h1>
 				     		<div>{project.description}</div>
 				     		<p>{project.lookingFor}</p>
@@ -38,6 +56,8 @@ export default class Swipe extends React.Component {
 			     		</div>
 		     		)})
 		      }
+		     	<button type="button" onClick={this.handleDislike}>Dislike</button>
+		     	<button type="button" onSubmit={this.onSubmit}>Like</button>
 	     		<button type="button"><Link to={`profile`}>Profile</Link></button>
 	     	</div>
      	</div>
