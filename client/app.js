c5536f9a9ddc20ab82f5da78a6989021515888e8
Tom LeConey <tleconey@gmail.com>
2016-07-29 12:25:11 -0500

    have reactive unliked/liked displying on skills page, need to now list then populate, then style

--- client/app.js
+++ client/app.js
@@ -4,14 +4,15 @@ import { Router, Route, browserHistory } from 'react-router';
 import Landing from './components/landing';
 import Swipe from './components/swipe';
 import Profile from './components/profile';
+import Skills from './components/skills'
 
 export default function App() {
   return (
     <Router history={browserHistory}>
       <Route path='/' component={Landing} />
-      <Router path='/skills' component={Skills} />
       <Route path='/swipe' component={Swipe} />
       <Route path='/profile' component={Profile} />
+      <Router path='/skills' component={Skills} />
     </Router>
   );
 }
--- client/components/Skills.js
+++ client/components/Skills.js
@@ -1,8 +1,9 @@
-import React, {Component} from 'react'
+import React from 'react'
+import { browserHistory, Link } from 'react-router';
+import fetch from 'isomorphic-fetch';
 
 
-
-export default class SkillsList extends Component {
+export default class SkillsList extends React.Component {
 	constructor () {
 		super ();
 		this.state = {
@@ -10,16 +11,33 @@ export default class SkillsList extends Component {
 		};
 		this.handleClick = this.handleClick.bind(this);
 	}
+
+
 	handleClick() {
 		this.setState({liked: !this.setState.liked});
 	}
+
+
 	render() {
 		const text=this.state.liked ? 'liked' : 'haven\'t liked';
+
 		return (
 			<div onClick = {this.handleClick}>
-				You {text} this. Click to toggle.
+				You {text} this.
 			</div>
 		);
 	}
-}
+};
 
+
+// export default class SkillListRender extends React.Component{
+// 		render() {
+// 		return (
+// 			<ul>
+// 			{this.props.list.map(function(listValue){
+// 				return <li>{listValue}</li>
+// 			})}
+// 			</ul> ) } }
+
+// React.render(<SkillListRender list={[JavaScript, C++, Python, Java, Mithril]} />, document.getElementById('skill-list-render1'));
+// React.render(<SkillListRender list = {[React, Redux, Angular, Node.js, Express, Shennanigans]} />, document.getElementById('skill-list-render2'));
