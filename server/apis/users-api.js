ac2d56cdfc1f4f4bf82441e7ce25ced5201235f8
sdavisonswim <sdavisonswim@gmail.com>
2016-07-29 15:33:35 -0500

    started adding github api, still learning

--- server/apis/users-api.js
+++ server/apis/users-api.js
@@ -1,10 +1,18 @@
 var express = require('express')
 var User = require('../models/user')
+var fetch = require('isomorphic-fetch');
+var bodyParser = require('body-parser');
+
 
 
 var UsersAPI = module.exports = express.Router()
 
+
+app.use(bodyParser.json());
+
+
 UsersAPI.get('/users', function (req, res) {
+  
   User.all()
     .then(function (users) {
       res.status(200).send(users)
@@ -16,7 +24,13 @@ UsersAPI.get('/users', function (req, res) {
 })
 
 UsersAPI.get('/users/:username', function (req, res) {
-
+  fetch("https://api.github.com/users/:username", {
+    method: 'GET',
+    headers: {
+      Authorization: token access_token,
+      Accept: 'application/json'
+    }
+  })
   User.getUser(req.params.username)
   .then(function(user){
   	res.status(200).send(user)
