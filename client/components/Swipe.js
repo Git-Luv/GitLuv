d0ad8a34068ef410e6ea11e505e4a33da6410574
Kyhan Turner <kyhanpt@gmail.com>
2016-07-29 12:19:59 -0500

    update styling for swipe buttons

--- client/components/Swipe.js
+++ client/components/Swipe.js
@@ -46,25 +46,22 @@ export default class Swipe extends React.Component {
 	  return (
 	  	<div className='swipe'>
 	     	<div>
-		     	{ this.state.projects
-		     		.map((project) => {
-		     		return (
-		     			<div key={project.id} className='currentProject'>
-				     		<span className="project"><h1>{project.title}</h1></span>
-				     		<div className="description">
-				     			<h2>Project Description:</h2>
-				     			<p>{project.description}</p>
-				     			<h2>Looking For:</h2>
-					     		<p>{project.lookingFor}</p>
-					     		<h2>Required Skills:</h2>
-					     		<p>{project.skills}</p>
-				     		</div>
-			     		</div>
-		     		)})
-		      }
-		     	<button type="button" onClick={this.handleDislike}>Dislike</button>
-		     	<button type="button" onSubmit={this.onSubmit}>Like</button>
-	     		<button type="button"><Link to={`profile`}>Profile</Link></button>
+     			<div key={this.state.projects[0].id} className='currentProject'>
+		     		<span className="project"><h1>{this.state.projects[0].title}</h1></span>
+		     		<div className="description">
+		     			<h2>Project Description:</h2>
+		     			<p>{this.state.projects[0].description}</p>
+		     			<h2>Looking For:</h2>
+			     		<p>{this.state.projects[0].lookingFor}</p>
+			     		<h2>Required Skills:</h2>
+			     		<p>{this.state.projects[0].skills}</p>
+		     		</div>
+	     		</div>
+	     		<div className="buttons">
+			     	<button type="button" className="button-dislike pure-button" onClick={this.handleDislike}>Dislike</button>
+			     	<button type="button" className="button-like pure-button" onSubmit={this.onSubmit}>Like</button>
+						<Link className="button-profile pure-button" to={`profile`}>Profile</Link>
+	   			</div>
 	     	</div>
      	</div>
 	  )
