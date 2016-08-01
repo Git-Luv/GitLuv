af2e269b067b973157b65d9cca17b03ca3e1fbcb
Tom LeConey <tleconey@gmail.com>
2016-08-01 14:45:00 -0500

    begun working on hookup for putting data in DB when initial selection of skills is over

--- client/models/skills.js
+++ client/models/skills.js
@@ -1,6 +1,16 @@
 import fetch from 'isomorphic-fetch';
 
-export function storeUserSkills (selectedSkills) {
-	return 
-
+export function storeUserSkills (user, selectedSkills) {
+	return fetch('/api/users', {
+		method: 'PATCH', 
+		headers: {
+			'Content-Type': 'application/json'
+		}
+		body:
+		{
+			'user': 'selectedSkills'
+		}
+	})
+	.then(users => return response.json());
+	.catch(error => console.error(error));
 }
