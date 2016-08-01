d9524e1cbf20107c3a7c84e4ad2812373f7f8979
Tom LeConey <tleconey@gmail.com>
2016-08-01 11:53:57 -0500

    hooked up end of skills button/ show me projects page to dynamic array of userSkills

--- client/components/Skills.js
+++ client/components/Skills.js
@@ -11,49 +11,42 @@ export default class SkillsList extends React.Component {
 			userSkills: []
 		};
 		this.handleClick = this.handleClick.bind(this);
-		// console.log("stateALL", this.state)
+		console.log("stateALL", this.state)
 	}
 
-// on click push skill into empty 'skills' array
-		// but first check if it's there, like with index of and -1
-// then have 2nd function that pushes that array of user 'skills' to the databse when they click the done/next butotn on the bottom of the page
-
-//then 
 		
 
 	handleClick(skill) {
-		this.setState({liked: !this.setState.liked});
+		// this.setState({liked: !this.setState.liked});
 			let userSkills = this.state.userSkills;
 			let allSkills = this.state.allSkills 
 			console.log("allSkills", allSkills)
-			userSkills.push(skill)
+
+					userSkills.push(skill);
+
 			console.log("userSkills", userSkills)
 			console.log("I WAS CLICKED ON", skill + "   I am typeOf ", typeof skill)
-	}
-		// console.log('You clicked: ', i)
-	
+	}	
 
-	sendToDatabase(i) {
-
-		console.log("HIIIIII", i.key)
+	sendToDatabase(skillz) {
+		let userSkills = this.state.userSkills;
+		console.log("USERSKILLS OK BYE", userSkills)
 	}
 
 	render() {
-		const text=this.state.liked ? 'liked' : 'haven\'t liked';
-
+		let skillz = this.state.userSkills;
 		return (
 			<div>
 	     	<div className="skills">
 	     	<span>Skills:</span>
 	     		{this.state.allSkills.map((skill, i) => {
-	     			// console.log("skills", skill.name)
 	     			return(<div className="skill" key={i} onClick={this.handleClick.bind(this, skill)}>
 							{skill}
 						</div>)
 	     		})}
 	     	</div>
 	     		<div >
-   				 		<button className="button-skillsSelected" onClick={this.sendToDatabase}>Show Me Some Projects!</button>
+   				 		<button className="button-skillsSelected" onClick={this.sendToDatabase.bind(this, skillz)}>Show Me Some Projects!</button>
    					</div>
 	    </div>
 		);
