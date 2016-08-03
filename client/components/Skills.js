5b95d5f380358b6632793321e970187cadbfbddb
Tom LeConey <tleconey@gmail.com>
2016-08-03 14:49:25 -0500

    working skills

--- client/components/Skills.js
+++ client/components/Skills.js
@@ -4,6 +4,7 @@ import fetch from 'isomorphic-fetch';
 import * as modelSkills from '../models/users';
 import * as modelProfile from '../models/profile';
 let dc = require('delightful-cookies')
+let Popout = require('react-popout')
 
 export default class SkillsList extends React.Component {
 	constructor (props) {
@@ -12,10 +13,21 @@ export default class SkillsList extends React.Component {
 		this.state = {
 			allSkills: ["React", "Angular.js", "Redux", "Mithril", "Backbone", "Node.js", "Express", "Git", "Passport", "Socket.io", "Mongo", "Mongoose", "Test Driven Development", "Continuous Deployment", "Agile Methodology", "Waterfall Methodology", "OAuth", "PHP", "Postgress", "KNEX", "Browserify", "Webpack", "Grunt", "Gulp", "CSS", "HTML", "ES2015", "React Native", "React-Router", "C++", "Java", "Ruby", "Python", "Go", "Haskell", "Android", "iOS", "C#", "Machine Language(s)", "Ruby on Rails", "MEAN stack", "PERRN stack", "Heroku"],
 			userSkills: [],
-			user: ""
+			user: "",
+			// isPoppedOut: false
 		};
+		// this.popoutClosed = this.popoutClosed.bind(this)
+		// this.popout = this.popout.bind(this);
 		this.handleClick = this.handleClick.bind(this);
 	};
+
+	popout() {
+	  this.setState({isPoppedOut: true});
+	}
+
+	popoutClosed() {
+	  this.setState({isPoppedOut: false});
+	}
 
 	componentWillMount() {
 		let cookie = dc.get("AuthToken")
@@ -39,10 +51,12 @@ export default class SkillsList extends React.Component {
 	};
 
 	sendToDatabase(user, skillz) {
+		let Popout = this.state.isPoppedOut
 		let userSkillz = {skills: this.state.userSkills};
 		let userName = this.state.user;
 		if (userSkillz.skills.length < 1 || !userSkillz) {
 			alert("Please choose at least one skill")
+			this.setState({isPoppedOut: true})
 			console.log("LESS THAN 1")
 		} else {
 		modelSkills.updateUser(userName, userSkillz);
@@ -52,6 +66,15 @@ export default class SkillsList extends React.Component {
 
 	render() {
 		let skillz = this.state.userSkills;
+
+		// if (this.state.isPoppedOut) {
+		//       return (
+		//         <Popout  title='Window title' onClosing={this.popoutClosed}>
+		//           <div>Popped out content!</div>
+		//         </Popout>
+		//       );
+		//     } 
+
 		return (
 			<div>
 				<h2 className = "skillPageTitle"> Choose Your Top Skills </h2>
