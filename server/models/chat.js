166b7dbc6ffdf48b7b367e6f2019a1883686cf76
mccarthyist <TMcCarthy8@gmail.com>
2016-08-04 12:13:09 -0500

    initial commit

---
+++ server/models/chat.js
@@ -0,0 +1,57 @@
+var Chat = module.exports;
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+var Schema = mongoose.Schema;
+mongoose.connect('mongodb://gitluv:lolboi5@ds031965.mlab.com:31965/gitluv');
+
+var conn = mongoose.connection;
+conn.on('error', console.error.bind(console, 'connection error in chat:'));
+
+conn.once('open', function() {
+	console.log("chat running!!!")
+})
+
+var chatSchema = new Schema({
+	chatRoom:  String,
+	visionary: String,
+	developer: String,
+	messages:  Array,
+})
+
+var ChatCollection = mongoose.model('Chatcollection', chatSchema)
+
+Chat.createIfNotExists = function(attrs){
+
+	let chatRoom = attrs.chatRoom
+	delete attrs.chatRoom 
+
+	return ChatCollection.findOneAndUpdate({chatRoom: chatRoom}, attrs, {upsert: true}, function (err, doc) {
+		if(err){
+			console.log("!!!-----------------!!!", err)
+		} else {
+			console.log("chat room created between " + attrs.visionary + " and " + attrs.developer)
+		}
+	})
+}
+
+Chat.getChatroom = function(chatRoom){
+
+	return ChatCollection.findOne({chatRoom: chatRoom}, function (err, doc) {
+		if(err) console.log("!!!-----------------!!!", err)
+	})
+}
+
+Chat.updateChatroom = function(chatRoom, changedAttrs){
+	console.log("chat room: " + chatRoom + " and changedAttrs: " + changedAttrs)
+
+			return ChatCollection.findOneAndUpdate({chatRoom: chatRoom}, changedAttrs, function (err, doc) {
+				if(err){
+					console.log("!!!-----------------!!!", err)
+				} else {
+					console.log("update chat room:", chatRoom, "!")
+				}
+			})
+		}
+		.catch(err => console.log("error in chat model: ", err))
+
+}
