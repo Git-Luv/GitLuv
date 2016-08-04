531d024ad0dd27e4b64c89dbe25a73162a8e9cae
Tom LeConey <tleconey@gmail.com>
2016-08-04 15:03:40 -0500

    working react-sanfona with example implementation

--- client/components/Project.js
+++ client/components/Project.js
@@ -3,6 +3,7 @@ import { browserHistory, Link } from 'react-router';
 import Sidebar from './sidebar'
 import CreateProject from './createproject'
 import * as Projects from '../models/projects'
+import { Accordion, AccordionItem } from 'react-sanfona';
 
 var dc = require('delightful-cookies');
 
@@ -61,7 +62,18 @@ export default class Project extends React.Component {
 			<div>
 				<div>
 					<h2 className="usersWhoLikedTitle">These Users Like Your Project!</h2>
-
+					<Accordion>
+					                {[1, 2, 3, 4, 5].map((item) => {
+					                    return (
+					                        <AccordionItem title={`Item ${ item }`} slug={item} key={item}>
+					                            <div>
+					                                {`Item ${ item } content`}
+					                                {item === 3 ? <p><img src="https://cloud.githubusercontent.com/assets/38787/8015584/2883817e-0bda-11e5-9662-b7daf40e8c27.gif" /></p> : null}
+					                            </div>
+					                        </AccordionItem>
+					                    );
+					                })}
+					            </Accordion>
 
 					
 				</div>
