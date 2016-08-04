95e1e04b2c296f46a16743ed4d5666b87cbd7930
mccarthyist <TMcCarthy8@gmail.com>
2016-08-03 19:52:57 -0500

    created chat back-end model

---
+++ server/models/chat.js
@@ -0,0 +1,83 @@
+var Chat = module.exports;
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+var Schema = mongoose.Schema;
+mongoose.connect('mongodb://gitluv:lolboi5@ds031965.mlab.com:31965/gitluv');
+
+var conn = mongoose.connection;
+conn.on('error', console.error.bind(console, 'connection error in chat:'));
+
+conn.once('open', function() {
+	console.log("chat running!!!")
+})
+
+var chatSchema = new Schema({
+	chatRoom:              String,
+	visionary:             String,
+	developer:             String,
+	messagesFromVisionary: Array,
+	messagesFromDeveloper: Array,
+})
+
+var ChatCollection = mongoose.model('Chatcollection', chatSchema)
+
+Chat.createIfNotExists = function(attrs){
+
+	let chatRoom = attrs.chatRoom
+	delete attrs.chatRoom 
+
+	return ChatCollection.findOneAndUpdate({chatRoom: chatRoom}, attrs, {upsert: true}, function (err, doc) {
+		if(err){
+			console.log("!!!-----------------!!!", err)
+		} else {
+			console.log("chat room created between " + attrs.visionary + " and " + attrs.developer)
+		}
+	})
+}
+
+Chat.getChatroom = function(chatRoom){
+
+	return ChatCollection.findOne({chatRoom: chatRoom}, function (err, doc) {
+		if(err) console.log("!!!-----------------!!!", err)
+	})
+}
+
+Chat.editChatroom = function(chatRoom, changedAttrs){
+	console.log("chat room: " + chatRoom + " and changedAttrs: " + changedAttrs)
+
+	return Chat.getChatroom(chatRoom)
+		.then( function (chatRoomInfo){
+		
+			if(changedAttrs.messagesFromVisionary){
+				let newArr = []
+				for(let i = 0; i < changedAttrs.messagesFromVisionary.length; i++){
+
+					if(!(chatRoomInfo.messagesFromVisionary.indexOf(changedAttrs.messagesFromVisionary[i]) >= 0)){
+						newArr.push(changedAttrs.messagesFromVisionary[i])
+					}
+				}
+				changedAttrs.messagesFromVisionary = chatRoomInfo.messagesFromVisionary.concat(newArr)
+			}
+
+			if(changedAttrs.messagesFromDeveloper){
+				let newArr1 = []
+				for(let i = 0; i < changedAttrs.messagesFromDeveloper.length; i++){
+
+					if(!(chatRoomInfo.messagesFromDeveloper.indexOf(changedAttrs.messagesFromDeveloper[i]) >= 0)){
+						newArr1.push(changedAttrs.messagesFromDeveloper[i])
+					}
+				}
+				changedAttrs.messagesFromDeveloper = chatRoomInfo.messagesFromDeveloper.concat(newArr1)
+			}
+
+			return ChatCollection.findOneAndUpdate({chatRoom: chatRoom}, changedAttrs, function (err, doc) {
+				if(err){
+					console.log("!!!-----------------!!!", err)
+				} else {
+					console.log("update chat room:", chatRoom, "!")
+				}
+			})
+		})
+		.catch(err => console.log("error in chat model: ", err))
+
+}
