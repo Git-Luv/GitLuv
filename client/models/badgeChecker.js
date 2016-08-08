7b10bc7277ffb96c9fc3bf05d6c47bac53775d5b
Jordan Campbell <jordan@ignitionstudios.net>
2016-08-08 14:35:10 -0500

    Merged from Local Surface Directory

--- client/models/badgeChecker.js
+++ client/models/badgeChecker.js
@@ -3,20 +3,19 @@ import * as Projects from './projects'
 import fetch from 'isomorphic-fetch';
 
 export function checkProjects(username) {
-	
-	fetch('/api/badges')
-	.then(res => {
-		Promise.all([
-			Users.getUser(username), 
-			Projects.getAllUserProjects(username),
-			res.json()])
-		.then(data => {
-			let user = data[0],
-					projects = data[1],
-					badges = data[2];
+	Promise.all([
+		Users.getUser(username), 
+		Projects.getAllUserProjects(username), 
+	])
+	.then(data => {
+		let user = data[0],
+				projects = data[1];
+				
+		console.log(user, projects);
 
-			console.log(user, projects, badges);
-
+		fetch('/api/badges')
+		.then(res => {
+			console.log(res);
 		})
 	})
 }
