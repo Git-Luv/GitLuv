f56e0343f58246dba303cc95940580e1c674e0b0
Jordan Campbell <jcampbell1441@gmail.com>
2016-08-09 17:22:26 -0500

    Notification system almost complete

--- client/components/NotifySystem.js
+++ client/components/NotifySystem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { browserHistory, Link } from 'react-router';
 import * as NotifyModel from '../models/notifications';
+import * as Profile from '../models/profile';
 
 var dc = require('delightful-cookies');
 
@@ -8,17 +9,33 @@ export default class NotifySystem extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-
+			notifications: [],
 		}
 	}
 
 	componentWillMount() {
-		
+		Profile.getUserData(dc.get('AuthToken').value)
+		.then(res => {
+			NotifyModel.get(res.login)
+			.then(data => {
+				this.setState({ notifications: data })
+			})
+		})
 	}
 
 	render() {
 		return(
 			<div className="NotifySystemMenu">
+				<div className="notifications">
+					{ this.state.notifications.map(item => {
+						return (
+							<div className={ item.isRead ? "notification-read" : "notification-unread" }>
+								<span>{item.description}</span>
+								<span>{item.created}</span>
+							</div>
+						)
+					}) }
+				</div>
 			</div>
 		)
 	}
