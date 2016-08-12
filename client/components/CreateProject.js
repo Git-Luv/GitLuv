17a7869486b95ea914907d8976f99d55cd1c94a2
Jordan Campbell <jordan@ignitionstudios.net>
2016-08-11 19:12:08 -0500

    Fixed bugs

--- client/components/CreateProject.js
+++ client/components/CreateProject.js
@@ -54,7 +54,7 @@ export default class CreateProject extends React.Component {
 				req_skills:     [],
 				users_liked:    [],
 				users_disliked: [],
-			}, stage: 2 })
+			}, stage: 1 })
 		})
 	}
 
@@ -77,7 +77,7 @@ export default class CreateProject extends React.Component {
 					}, stage: 1 })
 				})
 				.catch(err => {
-					this.handleCreateRepo();
+					this.setState({ stage: 2 })
 				})
 		}
 	}
@@ -174,9 +174,9 @@ export default class CreateProject extends React.Component {
 							<form className="pure-form create-project">
 							    <fieldset className="pure-group">
 							    	<div className='pure-div-1-2'>The repo named {this.state.project.title} does not exist, we can create it for you!</div>
-							        <textarea className="pure-input-1-2" onChange={this.handleDescChange.bind(this)} value='' placeholder="Please enter a short description of the repository"></textarea>
+							        <textarea className="pure-input-1-2" onChange={this.handleDescChange.bind(this)} placeholder="Please enter a short description of the repository"></textarea>
 							    </fieldset>
-							    <button type="button" onClick={this.setState.bind(this, {stage: 1})} className="pure-button pure-input-1-2 pure-button-primary project-next">Next</button>
+							    <button type="button" onClick={this.handleCreateRepo.bind(this)} className="pure-button pure-input-1-2 pure-button-primary project-next">Next</button>
 						    </form>
 					</div>
 					)
